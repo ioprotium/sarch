476c98f1eaf5fec2b0dd58f059e946f3755c3b1c
Brian Mayo <ioprotium@gmai.com>
2018-12-06 20:14:56 -0300

    Initial service config
    API version and endpoint should be declared on default.yml file. Firebase configuration must be set up locally. Ignoring local firebase config.
    
    Typings for service config

---
+++ service/src/types.ts
@@ -0,0 +1,19 @@
+export interface IAPIEndPoints {
+  accounts: string;
+}
+
+export interface IFirebaseConfig {
+  projectId: string;
+  clientEmail: string;
+  privateKey: string;
+  baseUrl: string;
+}
+
+export interface IAPIConfig {
+  version: string;
+  endpoints: IAPIEndPoints;
+}
+
+export interface IAppConfig {
+  firebase: IFirebaseConfig;
+}
