39e6f80d2db3bfeeb8e9b109035b872a205de9f7
Brian Mayo <ioprotium@gmai.com>
2018-12-06 23:45:58 -0300

    Account validator middleware
    We should validate the email input and return specific messages for each error. The max length for "email" field is set to 254, should it be configurable?

---
+++ service/src/api/accounts/Accounts.validators.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import AccountModel from './Accounts.model';
+
+const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+const emailMaxLength = 254;
+
+export function validateAccount(req: Request, res: Response, next: any) {
+  const account = req.body as AccountModel;
+  if (!account.email) {
+    res.status(400).send('Bad Request: email is empty or null.');
+  } else if (account.email.length > emailMaxLength) {
+    res.status(400).send('Bad Request: email is too long.');
+  } else if (!emailRegex.test(account.email.toLowerCase())) {
+    res
+      .status(400)
+      .send(
+        `Bad Request: email "${account.email}" is not a valid email address.`
+      );
+  } else {
+    next();
+  }
+}
