fe2529df752bb26aee6328f4a59f11037c2541d9
Brian Mayo <ioprotium@gmai.com>
2018-12-09 19:36:24 -0300

    Sarch Logger
    This middleware will parse an array of arguments in order to support TAG based logs. Winston transport Daily Rotatory File will generate logs with Date formated names as sarch-YYMMDD.log.
    
    The logger will use the following format: [timestamp] [log level] [message]. All log prints must follow the following format in order to in order to facilitate debugging: [TAG] [Message], where TAG is the name of the module who print a log line.

---
+++ service/src/components/logger.ts
@@ -0,0 +1,78 @@
+import * as winston from 'winston';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+
+const timestampFormat = winston.format.timestamp({
+  format: 'HH:mm:ss'
+});
+
+const printFormat = winston.format.printf(
+  // do not print if we add info.level.toUpperCase(), maybe it is because the colors?
+  info => `${info.timestamp} [${info.level}] ${info.message}`
+);
+
+const winstonLogger = winston.createLogger({
+  transports: [
+    new DailyRotateFile({
+      filename: 'sarch-%DATE%.log',
+      datePattern: 'YYMMDD',
+      maxSize: '1m',
+      maxFiles: '7d',
+      dirname: 'logs',
+      format: winston.format.combine(timestampFormat, printFormat),
+      level: 'warning',
+      handleExceptions: true,
+      json: false
+    }),
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      format: winston.format.combine(
+        winston.format.colorize(),
+        timestampFormat,
+        winston.format.simple(),
+        printFormat
+      )
+    })
+  ]
+});
+
+class SarchLogger {
+  public debug(...args: any[]) {
+    winstonLogger.debug(this.parseArgs(args));
+  }
+
+  public info(...args: any[]) {
+    winstonLogger.info(this.parseArgs(args));
+  }
+
+  public warn(...args: any[]) {
+    winstonLogger.warn(this.parseArgs(args));
+  }
+
+  public error(...args: any[]) {
+    winstonLogger.error(this.parseArgs(args));
+  }
+
+  private parseArgs(data: any[]): string {
+    let line = '';
+    for (let i = 0; i < data.length; i++) {
+      line += this.parseObject(data[i]) + ' ';
+    }
+
+    return line;
+  }
+
+  private parseObject(obj: any) {
+    switch (typeof obj) {
+      case 'number':
+      case 'string':
+        return obj;
+      case 'object':
+        return JSON.stringify(obj);
+      default:
+        return obj;
+    }
+  }
+}
+const logger = new SarchLogger();
+export default logger;
