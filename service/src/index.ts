c89a4dbf23f66dff48abef109a4fa639fd931264
Brian Mayo <ioprotium@gmai.com>
2018-12-09 20:57:53 -0300

    Fix typo and set Https port for dev and prod environment

--- service/src/index.ts
+++ service/src/index.ts
@@ -4,17 +4,17 @@ import * as fs from 'fs';
 import app from './app';
 import logger from './components/logger';
 
-const HTPPS_PORT = process.env.NODE_ENV === 'dev' ? 8080 : 443;
+const HTTPS_PORT = process.env.NODE_ENV === 'dev' ? 8080 : 443;
 const httpsOptions = {
   key: fs.readFileSync(__dirname + '/../credentials/sarch-key.pem'),
   cert: fs.readFileSync(__dirname + '/../credentials/sarch-cert.pem')
 };
 
-https.createServer(httpsOptions, app).listen(443, () => {
-  logger.info('[Server] Https server listening on port ' + HTPPS_PORT);
+https.createServer(httpsOptions, app).listen(HTTPS_PORT, () => {
+  logger.info('[Server] Https server listening on port ' + HTTPS_PORT);
 });
 
 const PORT = config.get<number>('server.port');
 app.listen(PORT, () => {
-  console.log('[Server] Http server listening on port ' + PORT);
+  logger.info('[Server] Http server listening on port ' + PORT);
 });
