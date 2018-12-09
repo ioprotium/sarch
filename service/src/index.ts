48bd0c67144998ebe6f384799ddb4583ec342661
Brian Mayo <ioprotium@gmai.com>
2018-12-09 20:13:15 -0300

    Serving API over Http and Https
    The SSL certificates were generated with OpenSSL. See more at http://www.hacksparrow.com/express-js-https.html

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,7 +1,20 @@
 import * as config from 'config';
+import * as https from 'https';
+import * as fs from 'fs';
 import app from './app';
+import logger from './components/logger';
+
+const HTPPS_PORT = process.env.NODE_ENV === 'dev' ? 8080 : 443;
+const httpsOptions = {
+  key: fs.readFileSync(__dirname + '/../credentials/sarch-key.pem'),
+  cert: fs.readFileSync(__dirname + '/../credentials/sarch-cert.pem')
+};
+
+https.createServer(httpsOptions, app).listen(443, () => {
+  logger.info('[Server] Https server listening on port ' + HTPPS_PORT);
+});
 
 const PORT = config.get<number>('server.port');
 app.listen(PORT, () => {
-  console.log('Sarch service listening on port ' + PORT);
+  console.log('[Server] Http server listening on port ' + PORT);
 });
