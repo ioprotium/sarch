bc3755fa80c3a2a2bf1b42c564419d71a7862182
Brian Mayo <ioprotium@gmail.com>
2018-12-10 03:18:27 -0300

    Build script added
    This scripts has all the logic to run before deployments. First it cleans the service dist folder and public folder. Then build the service and client and generate the API documentation with redoc-cli.
    
    Using ShellJS to guarantee support for linux and windows.

---
+++ service/scripts/build.js
@@ -0,0 +1,35 @@
+const shell = require('shelljs');
+const TAG = '[Build]';
+
+shell.echo(TAG, 'Cleaning dist folder');
+shell.rm('-rf', ['dist', 'public']);
+
+shell.echo(TAG, 'Running service build');
+if (shell.exec('tsc', { silent: true }).code !== 0) {
+  shell.echo('Error building service');
+  shell.exit(1);
+}
+
+shell.echo(TAG, 'Generating docs');
+if (shell.exec('npm run docs', { silent: true }).code !== 0) {
+  shell.echo('Error generating docs');
+  shell.exit(1);
+}
+
+shell.echo(TAG, 'Moving docs');
+shell.mkdir('dist/api/docs');
+shell.mv('redoc-static.html', 'dist/api/docs/index.html');
+
+shell.echo(TAG, 'Running client build');
+if (
+  shell.exec('npm run build --prefix ../client', { silent: true }).code !== 0
+) {
+  shell.echo('Error building client');
+  shell.exit(1);
+}
+
+shell.echo(TAG, 'Copying client build');
+shell.mkdir('public');
+shell.cp('-Rf', '../client/dist/*', 'public');
+
+shell.echo(TAG, 'Build finished');
